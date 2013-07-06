5121ea01dbfb285007a3bfc7d4e25a01bc9b5734
JOPHESTUS <jophestus@jophest.us>
2013-07-07 00:50:56 +1000

    Replaced github link with button

--- index.php
+++ index.php
@@ -131,7 +131,7 @@
 
       
       <div class="footer">
-        <p>&copy; JOPHESTUS 2013 - <a href="https://github.com/JOPHESTUS/isthispersonaweso.me">GitHub Source</a></p>
+        <p>&copy; JOPHESTUS 2013 - <a href="https://github.com/JOPHESTUS/isthispersonaweso.me" class="btn btn-info"><img src="git.png"/></a></p>
       </div>
 
     </div> <!-- /container -->
