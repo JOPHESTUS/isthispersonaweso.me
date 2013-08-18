d4a6f28d912723551acaa61d8d9c5a5c8acd7fcf
JOPHESTUS <jophestus@jophest.us>
2013-08-18 10:47:44 +1000

    Commit

--- awesomestate/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$database = "";  // the name of the database.
-$server = "";  // server to connect to.
-$db_user = "";  // mysql username to access the database with.
-$db_pass = "";  // mysql password to access the database with.
-$table = "";    // the table that this script will set up and use.
-$link = mysql_connect($server, $db_user, $db_pass);
-mysql_select_db($database,$link);
-?>
